db04336a0f378c9af3614f80e7a402ef0c78d022
Shristi Sarkar <133097728+shristisarkar@users.noreply.github.com>
2024-07-28 15:11:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section-title");
+
+    sections.forEach((title, index) => {
+        setTimeout(() => {
+            title.classList.add("visible
